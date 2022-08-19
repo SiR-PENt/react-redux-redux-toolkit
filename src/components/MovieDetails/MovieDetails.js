d4bae5ff2bb1ab4aa08e1ff0021b98509022b81f
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2022-08-19 12:44:19 +0100

    movie details page loaded with API

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,21 +1,83 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from '../../features/movies/movieSlice';
+import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow, removeSelectedMovieOrShow } from '../../features/movies/movieSlice';
+import './MovieDetails.scss'
 
 const MovieDetails = () => {
   const { imdbID } = useParams();
   const dispatch = useDispatch();
-  const data = useSelector(getSelectedMovieOrShow);
-  console.log(data);
   
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+    return () => {
+      dispatch(removeSelectedMovieOrShow())
+    }
   }, [dispatch, imdbID]);
+  
+  const data = useSelector(getSelectedMovieOrShow);
 
   return (
-    <div>MovieDetails</div>
-  )
+    <div className="movie-section">
+       {
+        Object.keys(data).length === 0 ?
+        <div>...Loading</div> :
+       ( <>
+      <div className="section-left">
+        <div className="movie-title">{data.Title}</div>
+        <div className="movie-rating">
+          <p>
+            IMDB rating <span>:{data.imdbRating} </span>
+          </p>
+
+          <p>
+            IMDB Votes <span>:{data.imdbVotes} </span>
+          </p>
+
+          <p>
+            RunTime <span>:{data.imdbRuntime} </span>
+          </p>
+
+          <p>
+            IMDB Year <span>:{data.imdbYear} </span>
+          </p>
+        </div>
+        <div className="movie-plot">{data.Plot}</div>
+        <div className="movie-info">
+          <div>
+            <span>Director</span>
+            <span>{data.Director}</span>
+          </div>
+
+          <div>
+            <span>Stars</span>
+            <span>{data.Actors}</span>
+          </div>
+
+          <div>
+            <span>Genres</span>
+            <span>{data.Genre}</span>
+          </div>
+
+          <div>
+            <span>Languages</span>
+            <span>{data.Language}</span>
+          </div>
+
+          <div>
+            <span>Awards</span>
+            <span>{data.Awards}</span>
+          </div>
+        </div>
+      </div>
+      <section className='section-right'>
+        <img src={data.Poster} alt={data.Title}/>
+      </section>
+        </>
+        )
+       }
+    </div>
+  );
 }
 
 export default MovieDetails
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -51,14 +51,15 @@ const movieSlice = createSlice({
   reducers: {
     // in toolkit, the state is mutated for you under the hood. You dont have to do it manually as in useReducer or redux. Hence, you don't have to do all the {...state, movies:'new movies'} bla bla bla. Redux toolkit uses immer to mutate state
 
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
 
       // here, lets say movieSlice actions is equal to reducers which is an object that contains addMovies.
 
-      // addMovies is an action creator function that takes initial state and action(payload has been destructure here) as parameters.
-    },
-  },
+      // addMovies is an action creator function that takes initial state and action(payload has  been des tructure here) as parameters.
+
+    removeSelectedMovieOrShow: (state) => {
+      state.selectedMovieOrShow = {};
+    }
+  }, 
   extraReducers: {
     [fetchAsyncMovies.pending]: () => {
       console.log("Pending");
@@ -83,7 +84,7 @@ const movieSlice = createSlice({
 });
 
 //In toolkit, action types are created based on reducer names, so let's say in conventional redux, this will be if(action.type) === addMovies.
-export const { addMovies } = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 
 //to get a value from the store, syntax:(state, name of the reducer, name of the initialState key). Take it as state is equal to the initialState
 export const getAllMovies = (state) => state.movies.movies;
